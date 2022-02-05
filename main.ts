16af1e83a83e0fae6dbd7bbbc0290c5f9cd2482d
manelballesteros <57194489+manelballesteros@users.noreply.github.com>
2022-02-05 23:12:16 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 let bruixola = 0
 let temperatura = 0
-radio.onReceivedNumber(function (receivedNumber) {
-	
-})
-radio.onReceivedValue(function (name, value) {
+radio.onReceivedString(function (receivedString) {
     bruixola = bruixola
+    basic.showString("" + (bruixola))
+    basic.pause(1000)
     temperatura = temperatura
+    basic.showString("" + (temperatura))
+    basic.pause(1000)
 })
 basic.forever(function () {
     radio.setGroup(1)
