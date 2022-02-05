3365d172f2419ca0853f0118fd1684fbc099bf5e
manelballesteros <57194489+manelballesteros@users.noreply.github.com>
2022-02-05 23:18:37 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ let bruixola = 0
 let temperatura = 0
 radio.onReceivedString(function (receivedString) {
     bruixola = bruixola
+    radio.sendValue("bruixola", bruixola)
     basic.showString("" + (bruixola))
     basic.pause(1000)
     temperatura = temperatura
+    radio.sendValue("temperatura", temperatura)
     basic.showString("" + (temperatura))
     basic.pause(1000)
 })
