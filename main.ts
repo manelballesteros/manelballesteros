e045bb673a30232a75303c3dd8ddebf09ff5100d
manelballesteros <57194489+manelballesteros@users.noreply.github.com>
2022-02-05 20:28:05 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    serial.writeValue("Graus", input.temperature())
+    serial.writeValue("llum", input.lightLevel())
+})
+basic.forever(function () {
+    basic.showString("" + (input.temperature()))
+    basic.pause(1000)
+    basic.showString("" + (input.lightLevel()))
+    basic.pause(1000)
+})
