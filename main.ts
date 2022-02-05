d8ef2d9fb95437cbeeb6d60a1c67d5347a8b4faf
manelballesteros <57194489+manelballesteros@users.noreply.github.com>
2022-02-05 23:01:35 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,5 @@
-let temperatura = 0
 let bruixola = 0
-radio.onReceivedString(function (receivedString) {
-    serial.writeValue("Graus", temperatura)
-    basic.showString("" + (temperatura))
-    basic.pause(1000)
-    serial.writeValue("bruixola", bruixola)
-    basic.showString("" + (bruixola))
-    basic.pause(1000)
-})
+let temperatura = 0
 basic.forever(function () {
     radio.setGroup(1)
     bruixola = bruixola
